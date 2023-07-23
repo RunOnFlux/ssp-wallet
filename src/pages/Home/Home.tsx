3ecb14e56aff614bd7ff1916bf0ab155df0041e1
TheTrunk <tadeas.kmenta@zel.cash>
2023-07-23 11:28:59 +0700

    navbar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,33 @@ import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from '../../hooks';
 import { setXpubInitialState } from '../../store';
-import { Spin, Row, Col } from 'antd';
+import { Spin, Row, Col, Image } from 'antd';
 import './Home.css';
+import { LockOutlined, SettingOutlined } from '@ant-design/icons';
 import Key from '../../components/Key/Key';
+
+function Navbar() {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const logout = () => {
+    dispatch(setXpubInitialState());
+    navigate('/login');
+  };
+  return (
+    <div className="navbar">
+      <Row justify="space-evenly">
+        <Col span={4}>
+          <Image height={30} preview={false} src="/ssp-logo.svg" />
+        </Col>
+        <Col span={16}>Wallet 1</Col>
+        <Col span={4}>
+          <SettingOutlined style={{ fontSize: '16px', paddingRight: '6px' }} />{' '}
+          <LockOutlined style={{ fontSize: '16px' }} onClick={logout} />
+        </Col>
+      </Row>
+    </div>
+  );
+}
 
 function App() {
   const alreadyMounted = useRef(false); // as of react strict mode, useEffect is triggered twice. This is a hack to prevent that without disabling strict mode
@@ -26,6 +50,7 @@ function App() {
     }
     if (xpubKey) {
       console.log('Key already synchronised.');
+      // here generate addresses
       setIsLoading(false);
     }
   });
@@ -46,6 +71,7 @@ function App() {
       })();
     } else {
       console.log('Key synchronised.');
+      // here generate addresses
       setIsLoading(false);
     }
   };
@@ -55,17 +81,13 @@ function App() {
       {isLoading && <Spin size="large" />}
       {!isLoading && (
         <>
-          <Row>
-            <Col span={4}>logo</Col>
-            <Col span={16}>Wallet 1</Col>
-            <Col span={4}>lock</Col>
-          </Row>
-          header - logo, Wallet 1, settings, lock address 0.0 FLUX usd value
-          actions - send, receive transactions
+          <Navbar />
+          {/* header - logo, Wallet 1, settings, lock address 0.0 FLUX usd value
+          actions - send, receive transactions */}
         </>
       )}
-      {xpubWallet}
-      {xpubKey}
+      {/* {xpubWallet}
+      {xpubKey} */}
       <Key derivationPath="xpub-48-19167-0-0" synchronised={keySynchronised} />
     </>
   );
