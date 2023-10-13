8c910de28083c716c9ebc7ef2eabf0bd004aa9cc
TheTrunk <tadeas.kmenta@zel.cash>
2023-10-13 21:51:10 +0200

    fix tx decoding

--- src/lib/transactions.ts
+++ src/lib/transactions.ts
@@ -199,38 +199,64 @@ export function decodeTransactionForApproval(
   rawTx: string,
   chain: keyof cryptos,
 ) {
-  const libID = getLibId(chain);
-  const network = utxolib.networks[libID];
-  const txhex = rawTx;
-  const txb = utxolib.TransactionBuilder.fromTransaction(
-    utxolib.Transaction.fromHex(txhex, network),
-    network,
-  );
-  console.log(JSON.stringify(txb));
-  let txReceiver = '';
-  let amount = '0';
-  let senderAddress = '';
-  const scriptPubKey = utxolib.script.scriptHash.output.encode(
-    utxolib.crypto.hash160(txb.inputs[0].redeemScript),
-  );
+  try {
+    const libID = getLibId(chain);
+    const network = utxolib.networks[libID];
+    const txhex = rawTx;
+    const txb = utxolib.TransactionBuilder.fromTransaction(
+      utxolib.Transaction.fromHex(txhex, network),
+      network,
+    );
+    console.log(JSON.stringify(txb));
+    let txReceiver = 'decodingError';
+    let amount = '0';
+    let senderAddress = '';
 
-  senderAddress = utxolib.address.fromOutputScript(scriptPubKey, network);
-  txb.tx.outs.forEach((out: output) => {
-    if (out.value) {
-      const address = utxolib.address.fromOutputScript(out.script, network);
-      console.log(address);
-      if (address !== senderAddress) {
-        txReceiver = address;
-        amount = new BigNumber(out.value)
-          .dividedBy(new BigNumber(1e8))
-          .toFixed();
-      }
+    if (txb.inputs[0].witnessScript && txb.inputs[0].redeemScript) {
+      // p2sh-p2wsh
+      const scriptPubKey = utxolib.script.scriptHash.output.encode(
+        utxolib.crypto.hash160(txb.inputs[0].redeemScript),
+      );
+      senderAddress = utxolib.address.fromOutputScript(scriptPubKey, network);
+    } else if (txb.inputs[0].witnessScript) {
+      // p2wsh
+      const scriptPubKey = utxolib.script.witnessScriptHash.output.encode(
+        utxolib.crypto.sha256(txb.inputs[0].witnessScript),
+      );
+      senderAddress = utxolib.address.fromOutputScript(scriptPubKey, network);
+    } else {
+      // p2sh
+      const scriptPubKey = utxolib.script.scriptHash.output.encode(
+        utxolib.crypto.hash160(txb.inputs[0].redeemScript),
+      );
+      senderAddress = utxolib.address.fromOutputScript(scriptPubKey, network);
     }
-  });
-  const txInfo = {
-    sender: senderAddress,
-    receiver: txReceiver,
-    amount,
-  };
-  return txInfo;
+
+    txb.tx.outs.forEach((out: output) => {
+      if (out.value) {
+        const address = utxolib.address.fromOutputScript(out.script, network);
+        console.log(address);
+        if (address !== senderAddress) {
+          txReceiver = address;
+          amount = new BigNumber(out.value)
+            .dividedBy(new BigNumber(1e8))
+            .toFixed();
+        }
+      }
+    });
+    const txInfo = {
+      sender: senderAddress,
+      receiver: txReceiver,
+      amount,
+    };
+    return txInfo;
+  } catch (error) {
+    console.log(error);
+    const txInfo = {
+      sender: 'decodingError',
+      receiver: 'decodingError',
+      amount: 'decodingError',
+    };
+    return txInfo;
+  }
 }
