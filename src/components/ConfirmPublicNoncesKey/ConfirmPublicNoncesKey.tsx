f024d88e930801fe50b2ada20e03e02f920e6907
TheTrunk <tadeas.kmenta@zel.cash>
2024-07-03 21:12:47 +0800

    save nonces from qr code or manual input

--- src/components/ConfirmPublicNoncesKey/ConfirmPublicNoncesKey.tsx
+++ src/components/ConfirmPublicNoncesKey/ConfirmPublicNoncesKey.tsx
@@ -1,7 +1,15 @@
-import { Typography, Button, Space, Modal } from 'antd';
-import { ClockCircleOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { QRCode, Typography, Button, Space, Modal, Input, message } from 'antd';
+import { NoticeType } from 'antd/es/message/interface';
+const { TextArea } = Input;
 import { useTranslation } from 'react-i18next';
-const { Text } = Typography;
+import localForage from 'localforage';
+const { Paragraph, Text } = Typography;
+
+interface publicNonces {
+  kPublic: string;
+  kTwoPublic: string;
+}
 
 function ConfirmPublicNoncesKey(props: {
   open: boolean;
@@ -9,13 +17,37 @@ function ConfirmPublicNoncesKey(props: {
 }) {
   const { t } = useTranslation(['home', 'common']);
   const { open, openAction } = props;
+  const [keyInput, setKeyInput] = useState('');
+
+  const [messageApi, contextHolder] = message.useMessage();
+  const displayMessage = (type: NoticeType, content: string) => {
+    void messageApi.open({
+      type,
+      content,
+    });
+  };
 
   const handleOk = () => {
+    if (keyInput.length > 0) {
+      // process keyInput
+      const sspKeyPublicNonces = JSON.parse(keyInput) as publicNonces[];
+      void (async function () {
+        try {
+          await localForage.setItem('sspKeyPublicNonces', sspKeyPublicNonces);
+          // display message
+          displayMessage('success', t('home:confirmPublicNoncesKey.public_nonces_stored'));
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
+    setKeyInput('');
     openAction(false);
   };
 
   return (
     <>
+      {contextHolder}
       <Modal
         title={t('home:confirmPublicNoncesKey.confirm_public_nonces_key')}
         open={open}
@@ -24,11 +56,39 @@ function ConfirmPublicNoncesKey(props: {
         onCancel={handleOk}
         footer={[]}
       >
-        <Space direction="vertical" size={32} style={{ marginBottom: 16, marginTop: 16 }}>
-          <ClockCircleOutlined style={{ fontSize: '48px' }} />
-          <Text>{t('home:confirmPublicNoncesKey.confirm_public_nonces_key_info')}</Text>
-          <Text>{t('home:confirmPublicNoncesKey.confirm_public_nonces_key_info_2')}</Text>
-          <Text>{t('home:confirmPublicNoncesKey.confirm_public_nonces_key_info_3')}</Text>
+        <Space
+          direction="vertical"
+          size="large"
+          style={{ marginBottom: 15, marginTop: 15 }}
+        >
+          <Text>
+            {t('home:confirmPublicNoncesKey.confirm_public_nonces_key_info')}
+          </Text>
+          <div>
+            <QRCode
+              errorLevel="M"
+              value="publicnonces"
+              icon="/ssp-logo-black.svg"
+              size={240}
+              style={{ margin: '0 auto 15px auto' }}
+            />
+            <Paragraph
+              copyable={{ text: 'publicnonces' }}
+              className="copyableAddress"
+            >
+              <Text>publicnonces</Text>
+            </Paragraph>
+          </div>
+          <Text>
+            {t('home:confirmPublicNoncesKey.confirm_public_nonces_key_info_2')}
+          </Text>
+
+          <TextArea
+            value={keyInput}
+            onChange={(e) => setKeyInput(e.target.value)}
+            placeholder={t('home:confirmPublicNoncesKey.manual_input')}
+            autoSize
+          />
           <Button type="primary" size="middle" onClick={handleOk}>
             {t('common:ok')}
           </Button>
