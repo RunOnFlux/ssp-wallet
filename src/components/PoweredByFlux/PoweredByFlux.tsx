6178710fe231436d8d53775f78b2f28f5b6e5ae8
TheTrunk <tadeas.kmenta@zel.cash>
2024-07-16 12:34:24 +0800

    adjust powered by flux

--- src/components/PoweredByFlux/PoweredByFlux.tsx
+++ src/components/PoweredByFlux/PoweredByFlux.tsx
@@ -1,14 +1,26 @@
 import { Image } from 'antd';
+import { useState } from 'react';
 
 interface Props {
   isClickeable?: boolean;
 }
 function PoweredByFlux({ isClickeable = false }: Props) {
-  const theme = window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'light'
-    : 'dark';
+  const darkModePreference = window.matchMedia('(prefers-color-scheme: dark)');
+  darkModePreference.addEventListener('change', (e) => changeTheme(e.matches));
+  const [themeStyle, setThemeStyle] = useState(
+    darkModePreference.matches ? 'light' : 'dark',
+  );
+  const [colorBox, setColorBox] = useState(darkModePreference.matches ? '#333' : '#ddd',);
 
-  const image = `/powered_by_${theme}.svg`;
+  const changeTheme = (isDark: boolean) => {
+    if (isDark) {
+      setThemeStyle('light');
+      setColorBox('#333');
+    } else {
+      setThemeStyle('dark');
+      setColorBox('#ddd');
+    }
+  }
 
   const open = (url: string) => {
     window.open(url, '_blank');
@@ -24,7 +36,7 @@ function PoweredByFlux({ isClickeable = false }: Props) {
         left: 0,
         right: 0,
         textAlign: 'center',
-        boxShadow: '0 -7px 7px -7px #ddd',
+        boxShadow: `0 -7px 7px -7px ${colorBox}`,
         padding: 10,
         paddingBottom: 14,
       }}
@@ -34,12 +46,18 @@ function PoweredByFlux({ isClickeable = false }: Props) {
         <Image
           height={18}
           preview={false}
-          src={image}
+          src={`/powered_by_${themeStyle}.svg`}
           onClick={() => open('https://runonflux.io')}
           style={{ cursor: 'pointer' }}
         />
       )}
-      {!isClickeable && <Image height={18} preview={false} src={image} />}
+      {!isClickeable && (
+        <Image
+          height={18}
+          preview={false}
+          src={`/powered_by_${themeStyle}.svg`}
+        />
+      )}
     </div>
   );
 }
