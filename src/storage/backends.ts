25c40b87363b9cb588562d3468c20e825ca4ca45
TheTrunk <tadeas.kmenta@zel.cash>
2024-07-23 15:34:52 +0800

    simplify backends file

--- src/storage/backends.ts
+++ src/storage/backends.ts
@@ -24,77 +24,58 @@ export function loadBackendsConfig() {
 loadBackendsConfig();
 
 // *** BACKENDS ***
-const flux = {
-  node: 'explorer.runonflux.io',
-};
-const fluxTestnet = {
-  node: 'testnet.runonflux.io',
-};
-const rvn = {
-  node: 'blockbookravencoin.app.runonflux.io',
-};
-const ltc = {
-  node: 'blockbooklitecoin.app.runonflux.io',
-};
-const btc = {
-  node: 'blockbookbitcoin.app.runonflux.io',
-};
-const doge = {
-  node: 'blockbookdogecoin.app.runonflux.io',
-};
-const zec = {
-  node: 'blockbookzcash.app.runonflux.io',
-};
-const bch = {
-  node: 'blockbookbitcoincash.app.runonflux.io',
-};
-const btcTestnet = {
-  node: 'blockbookbitcointestnet.app.runonflux.io',
-};
-const btcSignet = {
-  node: 'blockbookbitcoinsignet.app.runonflux.io',
-};
-const sepolia = {
-  node: 'node.ethereum-sepolia.runonflux.io',
-  api: 'api.ethereum-sepolia.runonflux.io/api',
-  explorer: 'sepolia.etherscan.io'
-};
-const eth = {
-  node: 'node.ethereum-mainnet.runonflux.io',
-  api: 'api.ethereum-mainnet.runonflux.io/api',
-  explorer: 'etherscan.io'
+const assetBackends: backends = {
+  flux: {
+    node: 'explorer.runonflux.io',
+  },
+  fluxTestnet: {
+    node: 'testnet.runonflux.io',
+  },
+  rvn: {
+    node: 'blockbookravencoin.app.runonflux.io',
+  },
+  ltc: {
+    node: 'blockbooklitecoin.app.runonflux.io',
+  },
+  btc: {
+    node: 'blockbookbitcoin.app.runonflux.io',
+  },
+  doge: {
+    node: 'blockbookdogecoin.app.runonflux.io',
+  },
+  zec: {
+    node: 'blockbookzcash.app.runonflux.io',
+  },
+  bch: {
+    node: 'blockbookbitcoincash.app.runonflux.io',
+  },
+  btcTestnet: {
+    node: 'blockbookbitcointestnet.app.runonflux.io',
+  },
+  btcSignet: {
+    node: 'blockbookbitcoinsignet.app.runonflux.io',
+  },
+  sepolia: {
+    node: 'node.ethereum-sepolia.runonflux.io',
+    api: 'api.ethereum-sepolia.runonflux.io/api',
+    explorer: 'sepolia.etherscan.io',
+  },
+  eth: {
+    node: 'node.ethereum-mainnet.runonflux.io',
+    api: 'api.ethereum-mainnet.runonflux.io/api',
+    explorer: 'etherscan.io',
+  },
 };
 
 export function backends() {
-  return {
-    flux: localForgeBackends?.flux || flux,
-    fluxTestnet: localForgeBackends?.fluxTestnet || fluxTestnet,
-    rvn: localForgeBackends?.rvn || rvn,
-    ltc: localForgeBackends?.ltc || ltc,
-    btc: localForgeBackends?.btc || btc,
-    doge: localForgeBackends?.doge || doge,
-    zec: localForgeBackends?.zec || zec,
-    bch: localForgeBackends?.bch || bch,
-    btcTestnet: localForgeBackends?.btcTestnet || btcTestnet,
-    btcSignet: localForgeBackends?.btcSignet || btcSignet,
-    sepolia: localForgeBackends?.sepolia || sepolia,
-    eth: localForgeBackends?.eth || eth,
-  };
+  const backendKeys = Object.keys(assetBackends);
+  const currentBackends: backends = backendKeys.reduce((acc, key) => {
+    acc[key] = localForgeBackends[key] || assetBackends[key];
+    return acc;
+  }, {} as backends);
+  return currentBackends;
 }
 
 export function backendsOriginal() {
-  return {
-    flux,
-    fluxTestnet,
-    rvn,
-    ltc,
-    btc,
-    doge,
-    zec,
-    bch,
-    btcTestnet,
-    btcSignet,
-    sepolia,
-    eth,
-  };
+  return assetBackends;
 }
