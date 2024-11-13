9cc1975371fb0c9ba8e21f7d4cb04e908006a8ca
TheTrunk <tadeas.kmenta@zel.cash>
2024-11-13 14:57:26 +0800

    add russian language fix selector showing wrong language

--- src/translations/resources/index.ts
+++ src/translations/resources/index.ts
@@ -2,3 +2,4 @@ export { default as en } from './en/_index';
 export { default as id } from './id/_index';
 export { default as cs } from './cs/_index';
 export { default as ph } from './ph/_index';
+export { default as ru } from './ru/_index';
