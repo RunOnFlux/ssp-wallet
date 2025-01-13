a2599b8e758d8b65cb9a6ffff90c169606e30961
TheTrunk <tadeas.kmenta@zel.cash>
2025-01-13 21:45:51 +0800

    translation

---
+++ src/translations/resources/tr/_index.ts
@@ -0,0 +1,15 @@
+import common from './common.json';
+import welcome from './welcome.json';
+import cr from './createrestore.json';
+import login from './login.json';
+import home from './home.json';
+import send from './send.json';
+
+export default {
+  common,
+  welcome,
+  cr,
+  login,
+  home,
+  send,
+};
