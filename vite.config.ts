b7c8036ea79056d5bbf505073a107b64b8ccec71
TheTrunk <tadeas@runonflux.io>
2025-03-20 19:13:06 +0800

    split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,52 @@ export default defineConfig({
       '@routes': '/src/routes',
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        // Enable code splitting
+        manualChunks: {
+          // Group React and related packages
+          'vendor-react': [
+            'react',
+            'react-dom',
+            'react-router',
+            'react-redux',
+            'react-i18next',
+            'react-secure-storage',
+          ],
+          // Group UI framework
+          'vendor-ui': ['antd', '@ant-design/icons'],
+          // Group blockchain/crypto libraries
+          'vendor-runonflux': ['@runonflux/utxo-lib', '@runonflux/flux-sdk'],
+          'vendor-crypto': ['@scure/bip32', '@scure/bip39', 'bchaddrjs'],
+          'vendor-eth': [
+            '@runonflux/aa-schnorr-multisig-sdk',
+            '@alchemy/aa-core',
+            'viem',
+          ],
+          // Group utilities
+          'vendor-utils': [
+            'axios',
+            'localforage',
+            'i18next',
+            'buffer',
+            'crypto-browserify',
+            'stream-browserify',
+            'bignumber.js',
+          ],
+        },
+        // Ensure consistent chunk naming for extension
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+    // Improve chunk loading by using dynamic imports
+    dynamicImportVarsOptions: {
+      warnOnError: true,
+    },
+  },
   define: {
     'process.env': {
       // mainly disable user agent to prevent
