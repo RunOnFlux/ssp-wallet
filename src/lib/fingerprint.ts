5dd5871d030eb27763eca2a3b17aa1f3d8db2826
TheTrunk <tadeas@runonflux.io>
2025-10-21 11:18:41 +0800

    adjust color to match ssp

--- src/lib/fingerprint.ts
+++ src/lib/fingerprint.ts
@@ -46,14 +46,14 @@ export function getFingerprint(extraText = ''): string {
       ctx.textBaseline = 'top';
       // The most common type
 
-      ctx.font = "14px 'Arial'";
+      ctx.font = "12px 'Arial'";
       ctx.textBaseline = 'alphabetic';
       ctx.fillStyle = '#f60';
       ctx.fillRect(125, 1, 62, 20);
       // Some tricks for color mixing to increase the difference in rendering
       ctx.fillStyle = '#069';
       ctx.fillText(txt, 2, 15);
-      ctx.fillStyle = 'rgba(102, 204, 0, 0.7)';
+      ctx.fillStyle = 'rgba(249, 219, 16, 0.9)'; // ssp color
       ctx.fillText(txt, 4, 17);
     }
     const canvasPrint = canvas.toDataURL();
